fba5a0cc0c3fcd988cbc5a5b12a03fcb9665a832
jyp90 <babungv@gmail.com>
2021-12-23 21:11:44 +0900

    CSS class usage

--- components/NavBars.js
+++ components/NavBars.js
@@ -8,8 +8,22 @@ export default function NavBars() {
 
     return <nav className={styles.nav}>
         <Link style={{color: "red"}} href="/">
-            <a style={{ color: router.pathname === '/' ? 'red' : 'blue'}}>Home</a>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
+          Home
+        </a>
             </Link>
-        <Link href="/about"><a>About</a></Link>
+        <Link href="/about">
+        <a className={[
+            styles.link,
+            router.pathname === "/about" ? styles.active : "",
+          ].join(" ")}
+        >
+            About
+        </a>
+        </Link>
     </nav>;
 }
